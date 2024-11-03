672984abab1ac0fab70a00ae188f6c8b479f1d4c
Predator2779 <66646630+Predator2779@users.noreply.github.com>
2024-11-03 18:56:41 +0500

    added player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,25 +5,42 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _rotationSpeed;
 
+    private PlayerControls _controls;
     private CharacterController _characterController;
     private Animator _animator;
     
     private void Awake()
     {
+        _controls = new PlayerControls();
         _characterController = GetComponent<CharacterController>();
         _animator = GetComponent<Animator>();
     }
 
+    private void OnEnable()
+    {
+        _controls.Gameplay.Enable();
+    }
+
     private void Update()
     {
-        var inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        var inputVector = _controls.Gameplay.Movement.ReadValue<Vector2>();
+        var movementVector = new Vector3(inputVector.x, 0, inputVector.y);
 
         _animator.SetBool("IsRun", inputVector.magnitude != 0);
         
         if (inputVector.magnitude == 0) return;
         
-        var movementVector = inputVector * _speed * Time.deltaTime;
-        _characterController.Move(movementVector);
+        Quaternion targetRotation = Quaternion.LookRotation(movementVector);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+        
+        var resultVector = movementVector * _speed * Time.deltaTime;
+        _characterController.Move(resultVector);
+    }
+
+    private void OnDisable()
+    {
+        _controls.Gameplay.Disable();
     }
 }
